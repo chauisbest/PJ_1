ea11e3c1c4b57240b8c8df4802d2e0078fc1268d
Chou Chou <chau28400@gmail.com>
2025-07-19 19:54:55 +0700

    ok

---
+++ PJ_1/Controllers/SanPhamController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PJ_1.Models;
+
+namespace PJ_1.Controllers
+{
+    public class SanPhamController : Controller
+    {
+        DB_ThuctapContext dB = new DB_ThuctapContext();
+        public IActionResult Index()
+        {
+            ViewBag.SanPham = dB.TblDmSanPhams
+                .Include(x=>x.DonViTinh)
+                .Include(x=>x.LoaiSanPham)
+                .Where(x=>x.SanPhamId > 1)
+                .ToList();
+            return View("IndexSP");
+        }
+    }
+}
